fad0e114d61fd6304140b01a7693e7334b3bad7f
Andrew Nguyen <andrewnhan1@gmail.com>
2020-01-22 10:25:42 -0500

    add *

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./components/ListComponent/list";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<TableList></TableList>, document.getElementById("root"));
 
