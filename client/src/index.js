248a18678890f48b2d444ab69a68155df250f8bf
Shankar Athinarayanan <shankar@ibm.com>
2020-01-22 10:20:33 -0500

    Don't need service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "./components/ListComponent/list";
 
 ReactDOM.render(<TableList></TableList>, document.getElementById("root"));
