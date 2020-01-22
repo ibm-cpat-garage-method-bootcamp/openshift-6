c822125cea99b6af747e248e1081592542b77ec4
omaribm <omar.hammami@ibm.com>
2020-01-22 10:20:58 -0500

    add test

---
+++ client/src/components/ItemComponent/item.test.js
@@ -0,0 +1,13 @@
+const item = require('./item.jsx');
+
+describe('item', ()=> {
+    test('canary', ()=> {
+        expect(true).toEqual(true);
+    });
+
+    test('I can input a name for the item', () => {
+        var curValue = item.value;
+        item.value = 'cereal';
+        expect(curValue).not.toEqual(item.value);
+    });
+});
