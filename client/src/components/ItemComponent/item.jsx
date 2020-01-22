f7fdfda4716d625dd4a5643b850b6b868a6b0039
popcor255 <popcor255@gmail.com>
2020-01-22 13:29:07 -0500

    push

--- client/src/components/ItemComponent/item.jsx
+++ client/src/components/ItemComponent/item.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyForm extends React.Component {
+export default class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { name: '', size: '',  comment: '' };
@@ -9,21 +9,40 @@ class MyForm extends React.Component {
 
   mySubmitHandler = (event) => {
     event.preventDefault();
-    alert("You are submitting " + this.state.name);
+    alert("You are submitting " + Object.entries(this.state));
+    console.log("You are submitting " + Object.entries(this.state));
   }
 
-  myChangeHandler = (event) => {
+  myNameHandler = (event) => {
     this.setState({name: event.target.value});
+  }
+
+  mySizeHandler = (event) => {
+    this.setState({size: event.target.value});
+  }
+
+  myCommentHandler = (event) => {
+    this.setState({comment: event.target.value});
   }
 
   render() {
     return (
       <form onSubmit={this.mySubmitHandler}>
+      <h1>Enter list</h1>
       <h1>Hello {this.state.name}</h1>
       <p>Enter your name, and submit:</p>
       <input
         type='text'
-        onChange={this.myChangeHandler}
+        id="name"
+        onChange={this.myNameHandler}
+      />
+      <input
+        type='text'
+        onChange={this.mySizeHandler}
+      />
+      <input
+        type='text'
+        onChange={this.myCommentHandler}
       />
       <input
         type='submit'
@@ -32,5 +51,3 @@ class MyForm extends React.Component {
     );
   }
 }
-
-export default MyForm;
--- client/src/components/ItemComponent/item.test.js
+++ client/src/components/ItemComponent/item.test.js
@@ -1,17 +1,36 @@
-import { MyForm } from './item.jsx';
-import { render } from '@testing-library/react';
+import MyForm from './item.jsx';
+import { render, fireEvent } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-15';
-import { mount } from 'enzyme';
+import {configure, mount, shallow} from 'enzyme'
+
 import React from 'react'
 
+
 describe('item', ()=> {
+
+    beforeAll(() => {
+        configure({ adapter: new Adapter() });
+    });
+
     test('canary', ()=> {
         expect(true).toEqual(true);
     });
 
     test('expect change name field change name state', () => {
-        var form = mount(<MyForm />);
-        form.simulate('change', { target: { value: 'abcdefg'} })
+        var dom = shallow(<MyForm />);
+        fireEvent.click(document.getElementById("name"));
+        expect(document.querySelector("input#name")).toBe('A');
+    });
+
+    test('expect change size field change size state', () => {
+        var form = render(<MyForm />);
+        form.find('input');
+        expect(form.state.name).toEqual('abcdefg');
+    });
+
+    test('expect change size field change size state', () => {
+        var form = render(<MyForm />);
+        form.find('input');
         expect(form.state.name).toEqual('abcdefg');
     });
 });
